1a94038fa08040bfdcd7d49ec4a3e19a570c7d24
Manu M P <manu.potti@mindcurv.com>
2025-11-28 23:16:50 +0530

    Created base class for producers

--- producers/models/producer.py
+++ producers/models/producer.py
@@ -1,7 +1,6 @@
-"""Producer base-class providing common utilites and functionality"""
+"""Producer base-class providing common utilities and functionality"""
 import logging
 import time
-
 
 from confluent_kafka import avro
 from confluent_kafka.admin import AdminClient, NewTopic
@@ -32,48 +31,78 @@ class Producer:
         self.num_replicas = num_replicas
 
         #
-        #
-        # TODO: Configure the broker properties below. Make sure to reference the project README
-        # and use the Host URL for Kafka and Schema Registry!
-        #
+        # Kafka + Schema Registry properties
         #
         self.broker_properties = {
-            # TODO
-            # TODO
-            # TODO
+            "bootstrap.servers": "localhost:9092",
+            "schema.registry.url": "http://localhost:8081",
         }
 
-        # If the topic does not already exist, try to create it
+        # Create topic if needed
         if self.topic_name not in Producer.existing_topics:
             self.create_topic()
             Producer.existing_topics.add(self.topic_name)
 
-        # TODO: Configure the AvroProducer
-        # self.producer = AvroProducer(
-        # )
+        #
+        # AvroProducer configuration
+        #
+        self.producer = AvroProducer(
+            {
+                "bootstrap.servers": self.broker_properties["bootstrap.servers"],
+                "schema.registry.url": self.broker_properties["schema.registry.url"],
+            },
+            default_key_schema=self.key_schema,
+            default_value_schema=self.value_schema,
+        )
 
     def create_topic(self):
         """Creates the producer topic if it does not already exist"""
-        #
-        #
-        # TODO: Write code that creates the topic for this producer if it does not already exist on
-        # the Kafka Broker.
-        #
-        #
-        logger.info("topic creation kafka integration incomplete - skipping")
+        logger.info(f"Checking/creating topic: {self.topic_name}")
+
+        admin_client = AdminClient(
+            {"bootstrap.servers": self.broker_properties["bootstrap.servers"]}
+        )
+
+        # Get existing topics from cluster
+        topic_metadata = admin_client.list_topics(timeout=10)
+
+        if self.topic_name in topic_metadata.topics:
+            logger.info(f"Topic '{self.topic_name}' already exists, skipping creation.")
+            return
+
+        logger.info(f"Topic: '{self.topic_name}' does not exist")
+
+        # Create new topic
+        new_topic = NewTopic(
+            self.topic_name,
+            num_partitions=self.num_partitions,
+            replication_factor=self.num_replicas,
+        )
+
+        fs = admin_client.create_topics([new_topic])
+
+        for topic, f in fs.items():
+            try:
+                f.result()
+                logger.info(f"Topic '{topic}' successfully created.")
+            except Exception as e:
+                logger.error(f"Failed to create topic '{topic}': {e}")
 
     def time_millis(self):
+        """Use this function to get the key for Kafka events"""
         return int(round(time.time() * 1000))
 
     def close(self):
         """Prepares the producer for exit by cleaning up the producer"""
-        #
-        #
-        # TODO: Write cleanup code for the Producer here
-        #
-        #
-        logger.info("producer close incomplete - skipping")
 
-    def time_millis(self):
-        """Use this function to get the key for Kafka Events"""
-        return int(round(time.time() * 1000))
+        if not hasattr(self, "producer") or self.producer is None:
+            logger.warning("Producer was not initialized. Nothing to close.")
+            return
+        
+        try:
+            self.producer.flush(10)
+            logger.info("Producer flush complete.")
+        except Exception as e:
+            logger.error(f"Error flushing producer: {e}")
+
+        logger.info("Producer closed.")
