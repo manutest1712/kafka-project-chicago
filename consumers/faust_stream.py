6caa6af220c252f7bb2073061070b2acbd863244
Manu M P <manu.potti@mindcurv.com>
2025-12-02 10:36:50 +0530

    faust stream completed

--- consumers/faust_stream.py
+++ consumers/faust_stream.py
@@ -1,14 +1,12 @@
 """Defines trends calculations for stations"""
 import logging
-
 import faust
-
 
 logger = logging.getLogger(__name__)
 
 
 # Faust will ingest records from Kafka in this format
-class Station(faust.Record):
+class Station(faust.Record, serializer="json"):
     stop_id: int
     direction_id: str
     stop_name: str
@@ -22,36 +20,81 @@ class Station(faust.Record):
 
 
 # Faust will produce records to Kafka in this format
-class TransformedStation(faust.Record):
-    station_id: int
-    station_name: str
-    order: int
-    line: str
-
-
-# TODO: Define a Faust Stream that ingests data from the Kafka Connect stations topic and
-#   places it into a new topic with only the necessary information.
-app = faust.App("stations-stream", broker="kafka://localhost:9092", store="memory://")
-# TODO: Define the input Kafka Topic. Hint: What topic did Kafka Connect output to?
-# topic = app.topic("TODO", value_type=Station)
-# TODO: Define the output Kafka Topic
-# out_topic = app.topic("TODO", partitions=1)
-# TODO: Define a Faust Table
-#table = app.Table(
-#    # "TODO",
-#    # default=TODO,
-#    partitions=1,
-#    changelog_topic=out_topic,
-#)
+class TransformedStation(faust.Record, serializer="json"):
+    station_id: int = 0
+    station_name: str = ""
+    order: int = 0
+    line: str = ""
 
 
 #
+# Faust App
 #
-# TODO: Using Faust, transform input `Station` records into `TransformedStation` records. Note that
-# "line" is the color of the station. So if the `Station` record has the field `red` set to true,
-# then you would set the `line` of the `TransformedStation` record to the string `"red"`
+app = faust.App(
+    "stations-stream",
+    broker="kafka://localhost:9092",
+    store="memory://",
+)
+
 #
+# Input topic from Kafka Connect
+# Kafka Connect output topic = topic.prefix + table name
+# In your config: topic.prefix = "org.city."
+# So stations table outputs to: org.city.stations
 #
+stations_topic = app.topic("org.city.stations", value_type=Station)
+
+#
+# Output topic
+#
+out_topic = app.topic("org.city.stations.transformed", partitions=1, value_type=TransformedStation)
+
+#
+# Table to store latest station info (not required for logic but required by assignment)
+#
+table = app.Table(
+    "stations_transformed",
+    default=TransformedStation,
+    partitions=1,
+    changelog_topic=out_topic,
+)
+
+# -------------------
+# Helper to determine line color
+# -------------------
+def resolve_line(station: Station) -> str:
+    if station.red:
+        return "red"
+    if station.blue:
+        return "blue"
+    if station.green:
+        return "green"
+    return "unknown"
+
+#
+# Stream processor
+#
+@app.agent(stations_topic)
+async def transform_station(stations):
+    async for station in stations:
+        print("in method")
+
+        transformed = TransformedStation(
+            station_id=station.station_id,
+            station_name=station.station_name,
+            order=station.order,
+            line=resolve_line(station),
+        )
+
+        key = str(transformed.station_id)
+        # Write to table (optional but part of assignment structure)
+        table[key] = transformed
+
+        # Produce to output topic
+        await out_topic.send(key=key, value=transformed)
+
+        logger.info(f"Transformed station: {transformed}")
+        print(f"Transformed station: {transformed}")
 
 
 if __name__ == "__main__":
